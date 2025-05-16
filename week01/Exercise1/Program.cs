d4cbf8671845d95048b148d4e0c277daf9c1cc76
FAB-SAM <samuelakinbola277@gmail.com>
2025-05-16 09:52:02 -0700

    week one

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("what is your first name?");
+        string firstname = Console.Readline();
+        Console.WriteLine("what is your last name?");
+        string lastname = Console.Readline();
+        Console.WriteLine($"Your name is "{lastname}" "{firstname}" "{lastname}" ");
     }
 }
