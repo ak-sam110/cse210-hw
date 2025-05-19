4714f9c4b2c553701061ff3deb0f0d21be47825e
FAB-SAM <samuelakinbola277@gmail.com>
2025-05-19 13:39:18 -0700

    week 1 Ex2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("what is your grade percentage?");
+        Console.Write("what is your grade percentage?");
         string answer = Console.ReadLine();
         int percentage = int.Parse(answer);
 
