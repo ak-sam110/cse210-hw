537dfec96e17c42cddaca30d34bd70f284ea71c5
FAB-SAM <samuelakinbola277@gmail.com>
2025-05-23 15:28:59 -0700

    ex4 wk1

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        list<int> numbers = new List<int>();
+        List<int> numbers = new List<int>();
         int number;
         Console.WriteLine("Enter a series of numbers (0 to stop):");
 
@@ -13,8 +13,9 @@ class Program
             Console.Write("Enter number:");
             number=int.Parse(Console.ReadLine());
             if (number !=0)
+
             {
-            number.Add(number);
+            numbers.Add(number);
             }
         } 
         while(number!=0);
@@ -24,7 +25,7 @@ class Program
         sum+=num;
         }
             Console.WriteLine($"Sum:{sum}");
-            double average = (double)sum / number.Count;
+            double average = (double)sum / numbers.Count;
             Console.WriteLine($"Average:{average}");
             int max = numbers[0];
             foreach (int num in numbers)
@@ -51,7 +52,7 @@ class Program
         {
             Console.WriteLine("No positive numbers were entered.");
         }
-            number.Sort();
+            numbers.Sort();
             Console.WriteLine("sorted list.");
             foreach(int num in numbers)
         {
