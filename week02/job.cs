0165bb9df3f4a99e890c79a40e9b721a26266470
FAB-SAM <samuelakinbola277@gmail.com>
2025-05-24 17:57:52 -0700

    wk2ex1

---
+++ week02/job.cs
@@ -0,0 +1,15 @@
+using system;
+{
+    public class job
+    {
+        public string _company;
+        public string _jobTitle;
+        public int _startYear;
+        public int _endYear;
+
+        public void Display()
+        {
+        Console.WriteLine($"{_company} {_jobTitle} {_startYear} {_endYear}"); 
+        }
+    }
+}
