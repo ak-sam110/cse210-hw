ca3cc7559cdba66ffbd2244fe810dfd7e97f90dc
FAB-SAM <samuelakinbola277@gmail.com>
2025-05-25 06:56:57 -0700

    wk2 ex

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,32 @@
 using System;
-
+class Job
+{
+    public string _jobTitle;
+    public string _company;
+    public int _startYear;
+    public int _endYear;   
+}
+    
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Fab Tech";
+        job1._startYear = 2021;
+        job1._endYear = 2023;
+
+        Console.WriteLine("job1 company: Fab Tech");
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Amazon";
+        job2._startYear = 2023;
+        job2._endYear = 2024;
+
+        Console.WriteLine("job2 company: Amazon");
+
     }
 }
+
--- week02/job.cs
+++ week02/job.cs
@@ -1,15 +1,32 @@
 using system;
 {
-    public class job
-    {
-        public string _company;
-        public string _jobTitle;
-        public int _startYear;
-        public int _endYear;
+    //job.cs
+    public class Job
+{
+    //members variables
+    public string _company;
+    public string _jobTitle;
+    public int _startYear;
+    public int _endYear;
 
-        public void Display()
-        {
-        Console.WriteLine($"{_company} {_jobTitle} {_startYear} {_endYear}"); 
-        }
+    public void DisplayJobDetails()
+    }
+
+{
+    Console.WriteLine($"{_company} {_jobTitle} {_startYear} {_endYear}");
+}
+    
+}
+    class Program
+{
+    static void Main(string[] args)
+    {
+        //create job objects
+        Job job1 = new Job("Software Engineer", "Fab Tech", 2021, 2023);
+        Job job2 = new Job("Senior Developer", "Amazon", 2023, 2024);
+
+        //Display job details
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
     }
 }
